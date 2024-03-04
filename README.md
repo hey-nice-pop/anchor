b3a05e16295d400d520d68dc83928562ae8c3ac1
hey-nice-pop <imo_watch@icloud.com>
2024-03-04 14:08:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Discord Bot
+wikipedia,weather,traffic...
+## 必要
+- discord.py
+- wikipedia-api
+```bash
+pip install wikipedia-api
+```
+- python-dotenv
+```bash
+pip install python-dotenv
+```
+- .env
+```.env
+BOT_TOKEN = 'YOURTOKEN' # bottoken
+```
+## 機能
+- Wikipedia
+- 天気予報(予定)
+- 交通情報(予定)
---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import config
+
+import game.minesweeper as minesweeper
+
+YOUR_BOT_TOKEN = config.BOT_TOKEN
+
+# インテントを有効化
+intents = discord.Intents.all()
+
+# Botオブジェクトの生成
+bot = commands.Bot(
+    command_prefix='/', 
+    intents=intents, 
+    sync_commands=True,
+    activity=discord.Game("テスト")
+)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f'ログイン完了: {bot.user}')
+
+# マインスイーパー機能のセットアップ
+minesweeper.setup(bot)
+
+# wiki.pyからコマンドをロード
+import wiki
+wiki.setup(bot)
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    # 他のコマンドも処理
+    await bot.process_commands(message)
+
+# Discordボットを起動
+bot.run(YOUR_BOT_TOKEN)
